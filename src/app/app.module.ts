790f5a9458f271b10f94321318294bbb386948ee
karanimaina <karani.maina2010@gmail.com>
2023-01-13 11:59:34 +0300

    add courses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
